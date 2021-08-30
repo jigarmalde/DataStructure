7bca611b6ab66041a724cf947a3b3075d301c07c
Jigar <jigar.malde123@gmail.com>
2021-08-31 00:12:13 +0530

    TOP View

--- DataStructure/src/main/java/com/ds/pratice/DataStructure/Classes/Tree/Pair.java
+++ DataStructure/src/main/java/com/ds/pratice/DataStructure/Classes/Tree/Pair.java
@@ -1,21 +1,20 @@
 package com.ds.pratice.DataStructure.Classes.Tree;
 
-public class Pair<U, V>
-{
-    public final U first;       // first field of a pair
-    public final V second;      // second field of a pair
+public class Pair<U,V> {
+        public final U first;       // first field of a pair
+        public final V second;      // second field of a pair
 
-    // Constructs a new Pair with specified values
+        // Constructs a new Pair with specified values
     private Pair(U first, V second)
-    {
-        this.first = first;
-        this.second = second;
-    }
+        {
+            this.first = first;
+            this.second = second;
+        }
 
-    // Factory method for creating a Typed Pair immutable instance
-    public static <U, V> Pair <U, V> of(U a, V b)
-    {
-        // calls private constructor
-        return new Pair<>(a, b);
-    }
+        // Factory method for creating a Typed Pair immutable instance
+        public static <U, V> Pair <U, V> of(U a, V b)
+        {
+            // calls private constructor
+            return new Pair<>(a, b);
+        }
 }
