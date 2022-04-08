085061c6b5d377e385c1ed0194914e141d7d424b
Jigar <jigar.malde123@gmail.com>
2022-04-08 10:38:34 +0530

    Graph

---
+++ DataStructure/src/main/java/com/ds/pratice/DataStructure/Classes/Graph/Vertex.java
@@ -0,0 +1,14 @@
+package com.ds.pratice.DataStructure.Classes.Graph;
+
+public class Vertex {
+	
+	public char label;
+	public boolean wasVisited;
+	public Vertex(char label) {
+		super();
+		this.label = label;
+		this.wasVisited= false;
+	}
+	
+
+}
